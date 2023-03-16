16b7fddca84681e2e2eaa600940c66f0a1f28710
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-03-16 06:08:02 -0700

    task5tg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A writing a c program that prints a line using a print f function
- *  Return:0(success)
+ * main - A c program that prints a line using a printf function
+ * Return: 0 (success)
  */
 int main(void)
-{ 
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return(0);
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
