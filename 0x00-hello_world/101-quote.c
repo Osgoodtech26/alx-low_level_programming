c1235b53194b7a7accb2f425a2829c8c04645244
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-03-16 08:27:48 -0700

    the last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return 1 (success)
+ * Return: 1 (success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
