1d7e1f21f5d7c308a3fa0d72e6dbe3eeebe9d092
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-03-20 05:00:15 -0700

    3-print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * alphabet in lowercas and in  uppercase
+ * Return : 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
