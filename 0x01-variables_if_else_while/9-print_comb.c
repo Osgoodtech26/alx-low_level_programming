b8d6f02c3fbca2f52ce1aeac9319a459984ee715
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-03-20 06:55:14 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main  - osgood
+ *  prints all possible combinations of single-digit numbers
+ *  Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
