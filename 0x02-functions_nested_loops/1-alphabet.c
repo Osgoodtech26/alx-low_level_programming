42fb0bcf11563bb736d0e031cb139828043eb048
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-03-22 10:04:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - osgood
+ * Write a function that prints the alphabets.
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
