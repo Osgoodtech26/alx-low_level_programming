76dad35f54cd76d38b9d98b286f0164fc8221916
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-03-27 02:58:30 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - take the pointer and update the value
+ * @n: int to check
+ * owned by osgood peter
+ * Return: 0 is success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
