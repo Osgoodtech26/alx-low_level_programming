62f0ac2b90459e1a0ba4cc085fe30484bf299c1f
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-04-05 19:08:09 -0700

    6-is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * check - checks for number that is prime
+ * @e:int
+ * @f:int
+ * Return:int
+ */
+int check(int e, int f)
+{
+	if (f < e / 2)
+	{
+		return (1);
+	}
+	if (e % f == 0)
+	{
+		return (0);
+	}
+	return (check(e, f + 1));
+}
+
+/**
+ * is_prime_number - states if number is prime
+ * @n:int
+ * Return:int
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	return (check(2, n));
+}
