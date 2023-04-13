2bd974cb46de2c7409563badb95f0532599a9b5e
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-04-12 19:15:16 -0700

    2-str_concat.cs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,6 @@
  * str_concat - concatenates two strings.
  * @s1: first string
  * @s2: second string
- *
  * Return: a pointer to a newly allocated space in memory which
  * contains the contents of s1, followed by the contents of s2,
  * and null terminated. NULL on failure
