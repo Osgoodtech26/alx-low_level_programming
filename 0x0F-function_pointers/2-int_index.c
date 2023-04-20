f410f1c86e473aa1a17d0c7f7738bea740a38809
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-04-20 02:44:03 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,16 +9,18 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+
 	int a = 0;
 
 	if (size > 0)
 	{
 		if (array != NULL && cmp != NULL)
 		{
-			while (a > size)
+			while (a < size)
 			{
 				if (cmp(array[a]))
 					return (a);
+
 				a++;
 			}
 		}
