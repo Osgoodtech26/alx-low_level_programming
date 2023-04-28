25283e488046a179f44a2e6e3af55f391bc243d3
osgoodtech26 <osgoodpeterodion@gmail.com>
2023-04-27 17:05:36 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the element of the list_t list
+ * @h: the list_t list
+ * owned by osgoodtech26
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+	count++;
+
+	h = h->next;
+
+	}
+
+	return (count);
+}
+
